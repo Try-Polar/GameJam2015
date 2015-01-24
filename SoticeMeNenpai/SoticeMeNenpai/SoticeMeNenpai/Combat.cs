d357fb7b47524e07fb38e963642141c261401a04
Elliot Bray <eb1033@york.ac.uk>
2015-01-24 13:02:57 +0000

    The project

---
+++ SoticeMeNenpai/SoticeMeNenpai/SoticeMeNenpai/Combat.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SoticeMeNenpai
+{
+    class Combat : Microsoft.Xna.Framework.Game
+    {
+        Vector2 playerStartPos = new Vector2(700, 350);
+        Sprite player = new Sprite();
+
+        Sprite enviroment = new Sprite();
+
+        Vector2 enemyStartPos = new Vector2(100, 50);
+        Sprite enemy = new Sprite();
+
+
+        public Combat(Texture2D playerText, Texture2D enemyText, Texture2D enviromentText)
+        {
+            player.SetTexture(playerText);
+            enviroment.SetTexture(enviromentText);
+            enemy.SetTexture(enemyText);
+
+            player.SetPosition(playerStartPos);
+            enemy.SetPosition(enemyStartPos);
+            enviroment.SetPosition(new Vector2();
+        }
+
+        public Sprite getPlayer()
+        {
+            return player;
+        }
+
+        public void CombatUpdate(GameTime gameTime)
+        {
+            KeyboardState state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.A))
+            {
+                player.SetPosition(player.GetPosition() + new Vector2(-5,0));
+            }
+            if (state.IsKeyDown(Keys.D))
+            {
+                player.SetPosition(player.GetPosition() + new Vector2(5, 0));
+            }
+        }
+
+        public void CombatDraw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(player.GetTexture(), player.GetRectangle(), Color.White);
+            spriteBatch.Draw(enemy.GetTexture(), enemy.GetRectangle(), Color.White);
+            spriteBatch.Draw(enviroment.GetTexture(), enviroment.GetRectangle(), Color.White);
+        }
+    }
+}
---
+++ SoticeMeNenpai/SoticeMeNenpai/SoticeMeNenpai/Game1.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SoticeMeNenpai
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Sprite Player = new Sprite();
+        Texture2D playerText;
+        Texture2D enemyText;
+        Texture2D enviromentText;
+
+
+        Combat firstRound;
+
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            
+
+            base.Initialize();
+        }
+
+
+        protected override void LoadContent()
+        {
+            playerText = Content.Load<Texture2D>("PlayerTexture");
+            enemyText = Content.Load<Texture2D>("PlayerTexture");
+            enviromentText = Content.Load<Texture2D>("firstRoundEnviroment");
+
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            firstRound = new Combat(playerText, playerText, playerText);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+            firstRound.CombatUpdate(gameTime);
+            
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            if (firstRound != null)
+            firstRound.CombatDraw(gameTime, spriteBatch);
+
+            spriteBatch.End();
+            
+
+            base.Draw(gameTime);
+        }
+    }
+}
